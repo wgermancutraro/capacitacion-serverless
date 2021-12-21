576acb14c01a107c5d953a63e55c3cdbf8e3ca1b
German Cutraro <german.cutraro@wolox.com.ar>
2021-12-21 14:31:01 -0300

    gift updates

--- functions/send_gift/service.js
+++ functions/send_gift/service.js
@@ -5,7 +5,6 @@ const db = new dynamodb.DocumentClient({ region: 'us-east-1' });
 const TABLE_NAME = process.env.CLIENTS_TABLE;
 
 exports.setClientGift = async (client) => {
-  console.log(client);
   const params = {
     TableName: TABLE_NAME,
     Key: {
